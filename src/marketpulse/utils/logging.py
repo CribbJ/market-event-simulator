07d9479f484cc24137e8e657c18f56f9fd256486
CribbJ <cribbj32@gmail.com>
2026-07-20 00:52:38 +0100

    project setup with logging, pydantic baseconfig and pyproject.toml

---
+++ src/marketpulse/utils/logging.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logging(level: str) -> None:
+    log_level = getattr(
+        logging,
+        level.upper(),
+        logging.INFO,
+    )
+
+    logging.basicConfig(
+        level=log_level,
+        format=(
+            "%(asctime)s "
+            "%(levelname)s "
+            "%(name)s - "
+            "%(message)s"
+        ),
+    )
