77b5a3766cdfb9284c6c1fdb8a214d8ea62a672f
CribbJ <cribbj32@gmail.com>
2026-08-19 23:14:33 +0100

    write parquet function

--- src/marketsimulator/commands/batch.py
+++ src/marketsimulator/commands/batch.py
@@ -2,10 +2,11 @@
 import json
 import argparse
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 from logging import Logger
 from typing import List
 
+import dask.dataframe as dd
 import pandas as pd
 
 from marketsimulator.generator import TradeGenerator
@@ -29,5 +30,33 @@ def generate_historic_trades(
         
     return [gen.generate() for _ in range(num_trades)]
 
+# Bump nparitiotns up 1 every ~100k-500k
+def write_parquet(trades: List, out_dir: Path, npartitions: int = 4) -> None:
+    df = pd.DataFrame(t.model_dump() for t in trades)
+    df["price"] = df["price"].astype(float)
+    df["fees"] = df["fees"].astype(float)
+    df["trade_date"] = pd.to_datetime(df["timestamp"]).dt.date.astype(str)
+    
+    ddf = dd.from_pandas(df, npartitions=npartitions)
+    
+    ddf.to_parquet(
+        out_dir,
+        engine="pyarrow",
+        partition_on=["symbol", "trade_date"],
+        write_index=False,
+    )
+    
+
 def run_batch(args: argparse.Namespace, logger: Logger) -> None:
-    pass
+    start_time = datetime.now() - timedelta(days=args.days_ago)
+    
+    trades = generate_historic_trades(
+        num_trades=args.num_trades,
+        start_time=start_time,
+        seed=args.seed
+    )
+    
+    out_dir = Path(args.out_dir)
+    write_parquet(trades, out_dir)
+    
+    logger.info(f"Generated {len(trades)} trades ({args.format}) -> {out_dir}")
